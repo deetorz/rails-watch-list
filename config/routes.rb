a29a1825cbf70cc7be84a8303e014806f5479fb3
Derek Torres <derek.n.torres@gmail.com>
2021-08-13 15:56:02 +0900

    added delete button

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get 'lists/new', to: 'lists#new', as: 'new_list'
   # post 'lists', to: 'lists#create'
   # get 'lists/:id', to: 'lists#show', as: 'list'
-  resources :lists, only: [:index, :new, :create, :show] do
-    resources :bookmarks, only: [:new, :create]
+  resources :lists, only: [:index, :new, :create, :destroy, :show] do
+    resources :bookmarks, only: [:new, :create, :destroy]
   end
 end
