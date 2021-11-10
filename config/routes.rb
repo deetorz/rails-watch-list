4aa0c06206f2fa69dea092a03f763ae87a58b52b
Derek Torres <derek.n.torres@gmail.com>
2021-11-10 13:42:04 +0900

    changed route for index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  # get 'lists', to: 'lists#index', as: 'lists'
-  # get 'lists/new', to: 'lists#new', as: 'new_list'
-  # post 'lists', to: 'lists#create'
-  # get 'lists/:id', to: 'lists#show', as: 'list'
-  resources :lists, only: [:index, :new, :create, :destroy, :show] do
-    resources :bookmarks, only: [:new, :create]
+  get '/', to: 'lists#index', as: 'lists'
+  resources :lists, only: %i[index new create destroy show] do
+    resources :bookmarks, only: %i[new create]
   end
 
   resources :bookmarks, only: [:destroy]
